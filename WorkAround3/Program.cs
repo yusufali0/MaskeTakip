68fa5d776b6e22a7de36dd01aa7bc9acb331c30a
yusufali0 <yusufali.bekci@hotmail.com>
2024-03-05 18:18:39 +0300

    Add project files.

---
+++ WorkAround3/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace WorkAround3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 =new Person();
+            person1.FirstName = "Yusuf Ali";
+            person1.LastName = "Bekçi";
+            person1.DateOfBirthYear = 1999;
+            person1.NationalIdentity = 123;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+            Console.ReadLine();
+        }
+    }
+}
